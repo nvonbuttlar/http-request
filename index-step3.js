34d9d25b8c0a320b865c407e3f030e76faa5494c
vagrant <nick.von.buttlar@gmail.com>
2018-10-01 19:40:25 +0000

    added some notes

---
+++ index-step3.js
@@ -0,0 +1,7 @@
+// Up to this point I have successfully written 2 versions
+// of an HTTPS client to retrieve and print HTML data
+// from the internet.
+
+// getAndPrintHTML can only work for the URL that has been
+// hardcoded. We should change our function to accept one or more parameters
+// so when we call it, we can specify what URL we want.
