ccdd69353fc7cf8e8d81d830ce5e1850bc184cc9
vagrant <nick.von.buttlar@gmail.com>
2018-10-01 18:33:23 +0000

    added index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response)) {
+
+  }
+
+
+
+
+}
