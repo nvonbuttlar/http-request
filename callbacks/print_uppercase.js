efd1ca2744ecd0671a98c804f9f1b389bff96b69
vagrant <nick.von.buttlar@gmail.com>
2018-10-01 22:05:31 +0000

    completed Step 6 and all three files associated

--- callbacks/print_uppercase.js
+++ callbacks/print_uppercase.js
@@ -1,1 +1,12 @@
-require('./http-functions');
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase(html) {
+  console.log(html.toUpperCase());
+}
+
+getHTML(requestOptions, printUpperCase);
