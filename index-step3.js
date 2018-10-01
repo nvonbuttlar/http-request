5d734df617dd9f56739a180ab89ab6ae25f4e2de
vagrant <nick.von.buttlar@gmail.com>
2018-10-01 19:58:43 +0000

    completed step 3, added option as a param to the print fn

--- index-step3.js
+++ index-step3.js
@@ -5,3 +5,38 @@
 // getAndPrintHTML can only work for the URL that has been
 // hardcoded. We should change our function to accept one or more parameters
 // so when we call it, we can specify what URL we want.
+
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML(options) {
+// Make sure to pass requestOptions as an argument when calling the fn
+// requestOptions needs global scope
+
+
+https.get(requestOptions, function(response) {
+
+  var total = '';
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      total += data;
+    });
+
+    response.on('end', function() {
+      console.log('Ending Request ' + total);
+    });
+
+
+  });
+}
+
+getAndPrintHTML(requestOptions);
+
+// END OF STEP 3
+
